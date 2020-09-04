21cb30ddea2f60ff55f489369baf08c3e1a7ed61
Diksha Gupta <diksha.gupta04@ad.infosys.com>
2020-09-04 21:13:04 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DIKSHA Edit <code>src/App.js</code>  hey you!!
         </p>
         <a
           className="App-link"
